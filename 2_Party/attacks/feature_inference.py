43a64058a2cdbc486cd1d9d18ad7811bef1a181d
Sizvy <sizvy06@gmail.com>
2025-05-01 01:25:17 +0100

    update

--- 2_Party/attacks/feature_inference.py
+++ 2_Party/attacks/feature_inference.py
@@ -7,35 +7,62 @@ class FeatureInferenceAttack:
     def __init__(self, target_feature_idx, emb_dim, device='cuda'):
         self.target_feature_idx = target_feature_idx
         self.discriminator = FeatureDiscriminator(emb_dim*2).to(device)
-        self.criterion = torch.nn.BCELoss()
-        self.optimizer = torch.optim.Adam(self.discriminator.parameters())
         self.device = device
+        self.optimizer = None
+        self.criterion = None
 
-    def train(self, active_embs, passive_embs, active_raw, epochs=100, batch_size=64):
+    def train(self, active_embs, passive_embs, active_raw, epochs=200, batch_size=256):
         unique_values = np.unique(active_raw[:, self.target_feature_idx])
         labels = np.array([1 if x in unique_values else 0 for x in active_raw[:, self.target_feature_idx]])
         
+        # Class-weighted loss
+        pos_weight = torch.tensor([len(labels)/sum(labels)-1]).to(self.device)
+        self.criterion = torch.nn.BCEWithLogitsLoss(pos_weight=pos_weight)
+        self.optimizer = torch.optim.AdamW(self.discriminator.parameters(), lr=0.001, weight_decay=1e-4)
+        scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(self.optimizer, T_max=epochs)
+
         dataset = TensorDataset(
-            torch.cat([torch.FloatTensor(active_embs), torch.FloatTensor(passive_embs)], dim=1),
+            torch.cat([torch.FloatTensor(active_embs), 
+                     torch.FloatTensor(passive_embs)], dim=1),
             torch.FloatTensor(labels)
         )
         loader = DataLoader(dataset, batch_size=batch_size, shuffle=True)
-        
-        for _ in range(epochs):
-            self._train_epoch(loader)
+
+        best_loss = float('inf')
+        patience = 10
+        patience_counter = 0
+
+        for epoch in range(epochs):
+            epoch_loss = self._train_epoch(loader)
+            scheduler.step()
+
+            if epoch_loss < best_loss:
+                best_loss = epoch_loss
+                patience_counter = 0
+            else:
+                patience_counter += 1
+                if patience_counter >= patience:
+                    break
 
     def _train_epoch(self, loader):
         self.discriminator.train()
+        epoch_loss = 0
         for inputs, labels in loader:
             inputs, labels = inputs.to(self.device), labels.to(self.device)
             self.optimizer.zero_grad()
             outputs = self.discriminator(inputs).squeeze()
             loss = self.criterion(outputs, labels)
             loss.backward()
+            torch.nn.utils.clip_grad_norm_(self.discriminator.parameters(), 1.0)
             self.optimizer.step()
+            epoch_loss += loss.item()
+        return epoch_loss/len(loader)
 
     def infer(self, active_emb, passive_emb):
         with torch.no_grad():
-            combined = torch.cat([torch.FloatTensor(active_emb), torch.FloatTensor(passive_emb)], dim=1).to(self.device)
+            combined = torch.cat([
+                torch.FloatTensor(active_emb), 
+                torch.FloatTensor(passive_emb)
+            ], dim=1).to(self.device)
             prob = self.discriminator(combined).item()
             return prob > 0.5, prob
--- 2_Party/models/discriminator_targeted_feature.py
+++ 2_Party/models/discriminator_targeted_feature.py
@@ -1,13 +1,16 @@
 import torch.nn as nn
-
 class FeatureDiscriminator(nn.Module):
     def __init__(self, input_dim):
         super().__init__()
         self.net = nn.Sequential(
-            nn.Linear(input_dim, 256),
-            nn.ReLU(),
+            nn.Linear(input_dim, 512),
+            nn.LeakyReLU(0.2),
+            nn.Dropout(0.3),
+            nn.Linear(512, 256),
+            nn.LeakyReLU(0.2),
+            nn.LayerNorm(256),  # Replaced BatchNorm
             nn.Linear(256, 128),
-            nn.ReLU(),
+            nn.LeakyReLU(0.2),
             nn.Linear(128, 1),
             nn.Sigmoid()
         )
