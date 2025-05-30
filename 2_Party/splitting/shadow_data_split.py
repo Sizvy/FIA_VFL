dcd733e0aee7423cec0b0facc8b45bb79655f2c9
Sizvy <sizvy06@gmail.com>
2025-05-30 10:31:17 +0100

    with 28 features

--- 2_Party/splitting/shadow_data_split.py
+++ 2_Party/splitting/shadow_data_split.py
@@ -9,7 +9,7 @@ df = pd.read_csv('../../../datasets/drive_cleaned.csv')
 df.columns = df.columns.astype(str)
 
 shadow_data_sz = 20000
-n = 32
+n = 33
 target_col = df.columns[-1]
 correlations = df.corr()[target_col].abs().sort_values(ascending=False)
 
